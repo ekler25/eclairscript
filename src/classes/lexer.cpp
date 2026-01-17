9343b0b0226bdbda74bbba39077e7b2f7048bd79
Ekler25 <134164161+ekler25@users.noreply.github.com>
2026-01-17 19:13:47 +0100

    lexer fire sigma

--- src/classes/lexer.cpp
+++ src/classes/lexer.cpp
@@ -1,22 +1,94 @@
 #include <iostream>
 
 #include <classes/lexer.hpp>
+#include <classes/tokenmap.hpp>
 #include <classes/token.hpp>
 
 #include <types.hpp>
+#include <utils.hpp>
 
-Vec<Token*> Lexer::LexicalAnalisys(String Data) {
-    Vec<Token*> ToReturn = {};
-    String CurrentToken = "";
+void Lexer::FlushCurrentToken() {
+    if (!CurrentToken.empty()) {
+        ToReturn.push_back(Token{
+            IdentifyToken(CurrentToken),
+            CurrentToken
+        });
+        CurrentToken.clear();
+    }
+}
 
-    for (auto& C : Data) {
-        switch (this->Mode) {
-            case LexerModes::Default:
+TokenTypes Lexer::IdentifyToken(String Value) {
+    if (TokenMap.find(Value) != TokenMap.end()) return TokenMap.at(Value);
+    if (IsInVector(Value, Keywords)) return TokenTypes::Keyword;
+    if (IsInVector(Value, Types)) return TokenTypes::Types;
+    if (IsValidSymbolName(Value)) return TokenTypes::Symbol;
+    if (IsANumber(Value)) return TokenTypes::Int;
+    return TokenTypes::Unknown;  
+}
+
+Token Lexer::PushToken(char ShortVariant, Vec<String> LongVariants, String& Data, int& Index) {
+    FlushCurrentToken();
+    String BestMatch{ShortVariant};
+    int BestLength = 1;
+
+    for (auto& Variant : LongVariants) {
+        int Length = static_cast<int>(Variant.length());
+        if (Index + Length > static_cast<int>(Data.size())) continue;
+
+        bool Match = true;
+        for (int i = 0; i < Length; i++) {
+            if (Data[Index + i] != Variant[i]) {
+                Match = false;
                 break;
-            case LexerModes::StringLiteral:
-                break;
+            }
+        }
+
+        if (Match && Length > BestLength) {
+            BestMatch = Variant;
+            BestLength = Length;
         }
     }
 
+    Index += BestLength - 1;
+
+    this->CurrentToken = "";
+    return Token{
+        IdentifyToken(BestMatch),
+        BestMatch
+    };
+}
+
+Vec<Token> Lexer::LexicalAnalisys(String Data) {
+    for (int i = 0; i < Data.length(); i++) {
+        switch (this->Mode) {
+            case LexerModes::Default:
+                if (isspace(Data[i])) {
+                    FlushCurrentToken();
+                    continue;
+                } else if (Data[i] == ';') {
+                    ToReturn.push_back(PushToken(';', {}, Data, i)); break;
+                } else {
+                    switch (Data[i]) {
+                        case '+': ToReturn.push_back(PushToken('+', {"++", "+="}, Data, i)); break;
+                        case '-': ToReturn.push_back(PushToken('-', {"--", "-=", "->"}, Data, i)); break;
+                        case '*': ToReturn.push_back(PushToken('*', {"**", "*="}, Data, i)); break;
+                        case '/': ToReturn.push_back(PushToken('/', {"/="}, Data, i)); break;
+                        case '=': ToReturn.push_back(PushToken('=', {"=>", "=="}, Data, i)); break;
+                        case '(': ToReturn.push_back(PushToken('(', {}, Data, i)); break;
+                        case ')': ToReturn.push_back(PushToken(')', {}, Data, i)); break;
+                        default: CurrentToken += Data[i];
+                    }
+                }
+                break;
+            case LexerModes::StringLiteral:
+                break;
+            default: break;
+        }
+    }
+
+    FlushCurrentToken();
+    for (auto& T : ToReturn) {
+        std::cout << "TOKEN: (Value: \x1b[92m" << T.Value << "\x1b[0m, Type: \x1b[92m" << (int)T.Type << "\x1b[0m)\n";
+    }
     return ToReturn;
 }
