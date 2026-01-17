052e574651b2c51d98bd356dcd8ba1377ea3a80c
Ekler25 <134164161+ekler25@users.noreply.github.com>
2026-01-17 22:02:31 +0100

    [U] Reading from files

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,37 @@
 #include <iostream>
+#include <fstream>
 #include <classes/lexer.hpp>
 #include <types.hpp>
 
-int main() {
-    String Data = "int test = (10 + 20) * 3;";
+int main(int argc, char** argv) {
+    if (!argv[1]) {
+        std::cout << "[ \x1b[91mUsage Error\x1b[0m ] Please provide a file name!\n";
+        return 1;
+    }
 
-    Lexer* LexerInstance = new Lexer();
-    LexerInstance->LexicalAnalisys(Data);
+    std::fstream File;
+
+    File.open(argv[1], std::ios::in);
+
+    if (!File.is_open()) {
+        std::cout << "[ \x1b[91mFile Error\x1b[0m ] Failed to open \x1b[91m" << argv[1] << "\x1b[0m!\n";
+        return 1;
+    }
+
+    Lexer LexerInstance;
+
+    String LineBuffer;
+    while (std::getline(File, LineBuffer)) {
+        if (!LineBuffer.empty() && LineBuffer.back() == '\r') LineBuffer.pop_back();
+        std::cout << "\nProcessing \x1b[92m" << LineBuffer << "\x1b[0m\n";
+
+        LexerInstance.Final.insert(
+            LexerInstance.Final.end(),
+            std::make_move_iterator(LexerInstance.ToReturn.begin()),
+            std::make_move_iterator(LexerInstance.ToReturn.end())
+        );
+        LexerInstance.ToReturn.clear();
+        LexerInstance.ToReturn.shrink_to_fit();  
+        LexerInstance.LexicalAnalisys(LineBuffer);
+    }
 }
